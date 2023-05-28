c261bcac6577221960ca115d1386ff9ac6d92289
Leonardo Cardoso <leonardo.cardoso@sptech.school>
2023-05-28 00:47:55 -0300

    Update Conexao.java

--- analyx-source-gui/src/main/java/com/br/api/banco/jdbc/Conexao.java
+++ analyx-source-gui/src/main/java/com/br/api/banco/jdbc/Conexao.java
@@ -14,21 +14,21 @@ public class Conexao {
     public Conexao() {
         BasicDataSource dataSource = new BasicDataSource();
 
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        //dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         
-        dataSource.setUrl("jdbc:mysql://localhost:3306/bd_analyx?autoReconnect=true&useSSL=false"); 
+        //dataSource.setUrl("jdbc:mysql://localhost:3306/bd_analyx?autoReconnect=true&useSSL=false"); 
         
-        dataSource.setUsername("Leonardo");
+        //dataSource.setUsername("admin");
         
-        dataSource.setPassword("2212Leo*");
+        //dataSource.setPassword("1234");
 
-//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//        
-//        dataSource.setUrl("jdbc:mysql://0.0.0.0:3306/bd_analyx?autoReconnect=true&useSSL=false"); 
-//        
-//        dataSource.setUsername("root");
-//        
-//        dataSource.setPassword("urubu100");
+       dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+      
+       dataSource.setUrl("jdbc:mysql://0.0.0.0:3306/bd_analyx?autoReconnect=true&useSSL=false"); 
+        
+       dataSource.setUsername("root");
+        
+       dataSource.setPassword("urubu100");
 //        
         this.conexaoDoBanco = new JdbcTemplate(dataSource);
     }
